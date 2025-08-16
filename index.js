116e8a40d97e278054319740f4ac28cc6cb7684a
Umang <raiumang0000@gmail.com>
2025-08-16 12:46:53 +0530

    Set route to / instead of /chat

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/chat', async (req, res) => {
+app.post('/', async (req, res) => {
     const userMessage = req.body.message;
 
     try {
